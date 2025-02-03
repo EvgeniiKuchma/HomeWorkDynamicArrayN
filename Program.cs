46998d34d994b2146f07ab6fbb9f5f71afc5cddf
Евгений Кучма <desferto87@gmail.com>
2025-02-03 22:58:51 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+namespace HomeWorkDynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandSumNumbers = "sum";
+            const string CommandExit = "exit";
+
+            int sumNumbers = 0;
+            int[] numberesArray = new int[0];
+
+            string inputUser;
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine("получение суммы всех чисел массива введите - sum");
+                Console.WriteLine("длы выхода введите - exit");
+                Console.WriteLine("-------------------------------------------");
+                Console.Write("Введите ваше число: ");
+
+                inputUser = Console.ReadLine();
+
+                if (inputUser == CommandSumNumbers || inputUser == CommandExit)
+                {
+                    switch (inputUser)
+                    {
+                        case CommandSumNumbers:
+                            for (int i = 0; i < numberesArray.Length; i++)
+                            {
+                                sumNumbers += numberesArray[i];
+                            }
+                            Console.WriteLine("\nСумма всех чисел: " + sumNumbers + "\n\n");
+                            break;
+
+                        case CommandExit:
+                            isOpen = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("\nневерная комманда\n");
+                            break;
+                    }
+                }
+                else
+                {
+                    int[] tempNumbers = new int[numberesArray.Length + 1];
+
+                    for (int i = 0; i < tempNumbers.Length; i++)
+                    {
+                        tempNumbers[i] = Convert.ToInt32(inputUser);
+                    }
+
+                    for (int i = 0; i < numberesArray.Length; i++)
+                    {
+                        tempNumbers[i] = numberesArray[i];
+                    }
+
+                    numberesArray = tempNumbers;
+
+                    Console.WriteLine("\nВаш массив: ");
+
+                    for (int i = 0; i < numberesArray.Length; i++)
+                    {
+                        Console.Write(numberesArray[i] + " ");
+                    }
+
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeWorkDynamicArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWorkDynamicArray")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a46fce95-e15c-45da-a2db-db94b6c7a50c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
